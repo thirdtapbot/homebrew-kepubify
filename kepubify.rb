2f6db7c131e1f266bf8804622e3bbee05c40a97b
Vítor Galvão <info@vitorgalvao.com>
2018-03-04 21:02:45 +0000

    Update kepubify.rb

--- kepubify.rb
+++ kepubify.rb
@@ -1,9 +1,9 @@
 class Kepubify < Formula
   desc "Convert ePubs into kepubs"
   homepage "https://github.com/geek1011/kepubify"
-  url "https://github.com/geek1011/kepubify/releases/download/v1.3.7/kepubify-darwin-64bit"
-  version "1.3.7"
-  sha256 "d21fd6f0afbfd69adbd0db31e91551ae8685d52ef53da513723a43d9c4b00d38"
+  url "https://github.com/geek1011/kepubify/releases/download/v2.0.0/kepubify-darwin-64bit"
+  version "2.0.0"
+  sha256 "0740b80d0b7dc4087a8ce3d8fcd44a83c16ffcaa73c9a7bd2dcbdb808f160f61"
 
   def install
     bin.install "kepubify-darwin-64bit" => "kepubify"
