d43669a5416b63a242774d9b2c160cc9bc59e0cb
Vítor Galvão <info@vitorgalvao.com>
2018-03-21 19:10:42 +0000

    Update kepubify.rb

--- kepubify.rb
+++ kepubify.rb
@@ -3,7 +3,7 @@ class Kepubify < Formula
   homepage "https://github.com/geek1011/kepubify"
   url "https://github.com/geek1011/kepubify/releases/download/v2.1.1/kepubify-darwin-64bit"
   version "2.1.1"
-  sha256 "f69d5b20570ff70096dd47558038f9de3f2491c288092dde3e985bea27821c28"
+  sha256 "373c5a6e28564cdb9fa554e8398229fcf728f86a957e2558b712df4be17816fc"
 
   def install
     bin.install "kepubify-darwin-64bit" => "kepubify"
